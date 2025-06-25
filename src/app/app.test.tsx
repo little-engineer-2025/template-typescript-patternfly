d8903f32ea4146151d7ff4fd4f6d4b217da51b1d
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-06-25 20:32:28 +0200

    fix: unit tests
    side effects need to be wrapped by `act`
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
 import App from '@app/index';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 describe('App tests', () => {
   test('should render default App component', () => {
-    const { asFragment } = render(<App />);
-
-    expect(asFragment()).toMatchSnapshot();
+    render(<App />);
+    expect(screen.getByRole('button', { name: 'Global navigation' })).toBeVisible();
   });
 
   it('should render a nav-toggle button', () => {
     render(<App />);
-
     expect(screen.getByRole('button', { name: 'Global navigation' })).toBeVisible();
   });
 
@@ -22,34 +20,32 @@ describe('App tests', () => {
   // using Cypress for this kind of test would be more efficient.
   it.skip('should hide the sidebar on smaller viewports', () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 600 });
-
     render(<App />);
-
-    window.dispatchEvent(new Event('resize'));
-
+    act(() => {
+      window.dispatchEvent(new Event('resize'));
+    });
     expect(screen.queryByRole('link', { name: 'Dashboard' })).not.toBeInTheDocument();
   });
 
   it('should expand the sidebar on larger viewports', () => {
     render(<App />);
-
-    window.dispatchEvent(new Event('resize'));
-
+    act(() => {
+      window.dispatchEvent(new Event('resize'));
+    });
     expect(screen.getByRole('link', { name: 'Dashboard' })).toBeVisible();
   });
 
   it('should hide the sidebar when clicking the nav-toggle button', async () => {
     const user = userEvent.setup();
-
     render(<App />);
-
-    window.dispatchEvent(new Event('resize'));
+    act(() => {
+      window.dispatchEvent(new Event('resize'));
+    });
     const button = screen.getByRole('button', { name: 'Global navigation' });
-
     expect(screen.getByRole('link', { name: 'Dashboard' })).toBeVisible();
-
-    await user.click(button);
-
+    await act(async () => {
+      await user.click(button);
+    });
     expect(screen.queryByRole('link', { name: 'Dashboard' })).not.toBeInTheDocument();
   });
 });
