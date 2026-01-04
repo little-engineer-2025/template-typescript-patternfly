4969da2293abc558c0829c610feaac07b68d65c9
Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>
2026-01-04 15:14:39 +0100

    fix: compose.mk rules
    - Clean-up not needed variables.
    - Not ignore deploy/ directory.
    - Add docker-compose file.
    
    Signed-off-by: Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>

--- scripts/mk/compose.mk
+++ scripts/mk/compose.mk
@@ -19,45 +19,20 @@ endif
 
 COMPOSE_FILE ?= $(PROJECT_DIR)/deploy/docker-compose.yaml
 
-COMPOSE_VARS_DATABASE=\
-	DATABASE_USER="$(DATABASE_USER)" \
-	DATABASE_PASSWORD="$(DATABASE_PASSWORD)" \
-	DATABASE_NAME="$(DATABASE_NAME)" \
-	DATABASE_EXTERNAL_PORT="$(DATABASE_EXTERNAL_PORT)"
-
-COMPOSE_VARS_DB_MIGRATE=\
-	DATABASE_USER="$(DATABASE_USER)" \
-	DATABASE_PASSWORD="$(DATABASE_PASSWORD)" \
-	DATABASE_NAME="$(DATABASE_NAME)" \
-	DATABASE_HOST="$(DATABASE_HOST)" \
-	DATABASE_PORT="$(DATABASE_EXTERNAL_PORT)"
-
-COMPOSE_VARS_KAFKA=\
-	KAFKA_CONFIG_DIR=$(KAFKA_CONFIG_DIR) \
-	KAFKA_DATA_DIR=$(KAFKA_DATA_DIR) \
-	ZOOKEEPER_CLIENT_PORT=$(ZOOKEEPER_CLIENT_PORT) \
-	KAFKA_TOPICS=$(KAFKA_TOPICS)
-
-COMPOSE_VARS_APP=\
-    APP_SECRET="$(APP_SECRET)" \
-    APP_VALIDATE_API=$(APP_VALIDATE_API) \
-    APP_TOKEN_EXPIRATION_SECONDS=$(APP_TOKEN_EXPIRATION_SECONDS)
+COMPOSE_VARS_APP=
 
 COMPOSE_VARS= \
 	CONTAINER_IMAGE_BASE="$(CONTAINER_IMAGE_BASE)" \
 	CONTAINER_IMAGE_TAG="$(CONTAINER_IMAGE_TAG)" \
-	$(COMPOSE_VARS_DATABASE) \
-	$(COMPOSE_VARS_KAFKA) \
 	$(COMPOSE_VARS_APP)
 
 
 .PHONY: compose-up
 compose-up: ## Start local infrastructure
-	source .venv/bin/activate && \
 	$(COMPOSE_VARS) \
 	    $(COMPOSE) -f $(COMPOSE_FILE) -p $(COMPOSE_PROJECT) up -d
-	$(MAKE) .compose-wait-db
-	$(MAKE) $(COMPOSE_VARS_DB_MIGRATE) db-migrate-up
+	@#$(MAKE) .compose-wait-db
+	@#$(MAKE) $(COMPOSE_VARS_DB_MIGRATE) db-migrate-up
 
 .PHONY: .compose-wait-db
 .compose-wait-db:
@@ -69,29 +44,24 @@ compose-up: ## Start local infrastructure
 
 .PHONY: compose-down
 compose-down: ## Stop local infrastructure
-	source .venv/bin/activate && \
 	$(COMPOSE_VARS) \
 	    $(COMPOSE) -f $(COMPOSE_FILE) -p $(COMPOSE_PROJECT) down --volumes
 
 .PHONY: compose-build
 compose-build: ## Build the images at docker-compose.yaml
-	source .venv/bin/activate && \
 	$(COMPOSE_VARS) \
 	    $(COMPOSE) -f $(COMPOSE_FILE) -p $(COMPOSE_PROJECT) build
 
 .PHONY: compose-pull
 compose-pull: ## Pull images
-	source .venv/bin/activate && \
 	$(COMPOSE_VARS) \
 	    $(COMPOSE) -f $(COMPOSE_FILE) -p $(COMPOSE_PROJECT) pull
 
 .PHONY: compose-logs
 compose-logs: ## Print out infrastructure logs
-	source .venv/bin/activate && \
 	$(COMPOSE_VARS) \
 	    $(COMPOSE) -f $(COMPOSE_FILE) -p $(COMPOSE_PROJECT) logs
 
 .PHONY: compose-clean
 compose-clean: compose-down  ## Stop and clean local infrastructure
-	source .venv/bin/activate && \
 	$(CONTAINER_ENGINE) volume prune --force
