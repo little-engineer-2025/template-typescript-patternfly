79f62babd278ef4d6778ed4935c626a3455c5c56
Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>
2026-01-04 13:52:50 +0100

    chore: clean-up variables
    Housekeeping by removing not used variables defined at
    scripts/mk/variables.mk file.
    
    Signed-off-by: Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>

--- scripts/mk/variables.mk
+++ scripts/mk/variables.mk
@@ -7,37 +7,19 @@
 ##
 APP_NAME ?= todo
 export APP_NAME
+APP_COMPONENT ?= frontend
+export APP_COMPONENT
 
 BIN ?= bin
 PATH := $(CURDIR)/$(BIN):$(PATH)
 export PATH
 
-CONFIG_PATH ?= $(PROJECT_DIR)/configs
-export CONFIG_PATH
-CONFIG_YAML := $(CONFIG_PATH)/config.yaml
-
 COMPOSE_FILE ?= $(PROJECT_DIR)/deploy/docker-compose.yaml
 
+ifeq ($(QUAY_USER),)
+error(QUAY_USER is not defined)
+endif
 CONTAINER_IMAGE_BASE ?= quay.io/$(firstword $(subst +, ,$(QUAY_USER)))/$(APP_NAME)-$(APP_COMPONENT)
 
 # Application specific parameters
-APP_EXPIRATION_TIME ?= 15
-export APP_EXPIRATION_TIME
-APP_PAGINATION_DEFAULT_LIMIT ?= 10
-export APP_PAGINATION_DEFAULT_LIMIT
-APP_PAGINATION_MAX_LIMIT ?= 100
-export APP_PAGINATION_MAX_LIMIT
-# Enable IS_FAKE_ENABLED for the ephemeral deployment
-APP_ACCEPT_X_RH_FAKE_IDENTITY ?= true
-export APP_ACCEPT_X_RH_FAKE_IDENTITY
-APP_VALIDATE_API ?= true
-export APP_VALIDATE_API
 
-# Set the default token expiration in seconds (2 hours)
-APP_TOKEN_EXPIRATION_SECONDS ?= 7200
-export APP_TOKEN_EXPIRATION_SECONDS
-
-# main secret for various MAC and encryptions like
-# domain registration token and encrypted private JWKs
-APP_SECRET ?= sFamo2ER65JN7wxZ48UZb5GbtDc053ahIPJ0Qx47bzA
-export APP_SECRET
